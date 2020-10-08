1eb6ebaa14ca394d8dbb9004f248b025ae13d52b
Urban <dzindzinovic@yahoo.com>
2020-10-08 12:13:12 +0200

    Initial commit

---
+++ TheLastTime/Data/ExampleDb.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.IndexedDB.Framework;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLastTime.Data
+{
+    // Represents the database
+    public class ExampleDb : IndexedDb
+    {
+        public ExampleDb(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) { }
+
+        // These are like tables. Declare as many of them as you want.
+        public IndexedSet<Person> People { get; set; }
+    }
+}
