074427f05f8198a2597caf4e30463d0895f3b02b
Urban <dzindzinovic@yahoo.com>
2020-10-08 12:05:09 +0200

    Initial commit

--- TheLastTime/Program.cs
+++ TheLastTime/Program.cs
@@ -1,3 +1,4 @@
+using Blazor.IndexedDB.Framework;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
@@ -30,6 +31,8 @@ namespace TheLastTime
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IIndexedDbFactory, IndexedDbFactory>();
 
             var host = builder.Build();
 
