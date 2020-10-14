d4c3a0607101295702e7fd2d6bf4e68305dfa0d1
Urban <dzindzinovic@yahoo.com>
2020-10-14 21:40:18 +0200

    Initial commit

---
+++ TheLastTime/Data/IndexedDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.IndexedDB.Framework;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLastTime.Data
+{
+    public class IndexedDatabase : IndexedDb
+    {
+        public IndexedDatabase(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) { }
+
+        public IndexedSet<Category> Categories { get; set; }
+        public IndexedSet<Event> Events { get; set; }
+        public IndexedSet<Instance> Instances { get; set; }
+    }
+}
