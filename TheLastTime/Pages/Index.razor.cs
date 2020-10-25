44f455f848c05cb009672d7232a047e0b35d37c5
Urban <dzindzinovic@yahoo.com>
2020-10-25 13:13:19 +0100

    Version 0.0.0.1

--- TheLastTime/Pages/Index.razor.cs
+++ TheLastTime/Pages/Index.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using TheLastTime.Data;
 
 namespace TheLastTime.Pages
@@ -18,5 +20,23 @@ namespace TheLastTime.Pages
 
         [Inject]
         DataService DataService { get; set; } = null!;
+
+        readonly Dictionary<string, ButtonSize> ButtonSizeDict = new Dictionary<string, ButtonSize>()
+        {
+            { "small", ButtonSize.Small },
+            { "medium", ButtonSize.None },
+            { "large", ButtonSize.Large }
+        };
+
+        ButtonSize ButtonSize => ButtonSizeDict[DataService.Settings.Size];
+
+        readonly Dictionary<string, Size> SizeDict = new Dictionary<string, Size>()
+        {
+            { "small", Size.Small },
+            { "medium", Size.None },
+            { "large", Size.Large }
+        };
+
+        Size Size => SizeDict[DataService.Settings.Size];
     }
 }
