6ace2023c6998ded534a22465692c4c99c815b32
Urban <dzindzinovic@yahoo.com>
2020-12-30 12:09:42 +0100

    Version 0.1.1.1

---
+++ TheLastTime/Shared/MainLayout.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel;
+using TheLastTime.Data;
+
+namespace TheLastTime.Shared
+{
+    public sealed partial class MainLayout : IDisposable
+    {
+        [Inject]
+        DataService DataService { get; set; } = null!;
+
+        [Inject]
+        State State { get; set; } = null!;
+
+        protected override void OnInitialized()
+        {
+            DataService.PropertyChanged += PropertyChanged;
+            State.PropertyChanged += PropertyChanged;
+        }
+
+        void PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            DataService.PropertyChanged -= PropertyChanged;
+            State.PropertyChanged -= PropertyChanged;
+        }
+    }
+}
