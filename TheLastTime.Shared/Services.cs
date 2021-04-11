3cfcf0213bdb269202a514ba4a1b2356a34257c1
Urban <dzindzinovic@yahoo.com>
2021-04-11 19:08:09 +0200

    Version 1.0.0.0

---
+++ TheLastTime.Shared/Services.cs
@@ -0,0 +1,50 @@
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using IndexedDB.Blazor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using TheLastTime.Shared.Data;
+
+namespace TheLastTime.Shared
+{
+    public static class Services
+    {
+        public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+
+            serviceCollection.AddScoped<IIndexedDbFactory, IndexedDbFactory>();
+
+            serviceCollection.AddScoped<IDatabaseAccess, DatabaseAccess>();
+
+            serviceCollection.AddScoped<JsInterop>();
+
+            serviceCollection.AddScoped<DataService>();
+
+            serviceCollection.AddScoped<GoogleDrive>();
+
+            serviceCollection.AddScoped<State>();
+
+            serviceCollection.AddScoped<ThemeOptions>();
+
+            return serviceCollection;
+        }
+
+        public static async Task UseServices(this IServiceProvider serviceProvider)
+        {
+            //serviceProvider.UseBootstrapProviders().UseFontAwesomeIcons(); // v0.9.2 ---> v0.9.3
+
+            await serviceProvider.GetRequiredService<DataService>().LoadData();
+
+            serviceProvider.GetRequiredService<GoogleDrive>(); // make sure that there is one instance of GoogleDrive
+        }
+    }
+}
