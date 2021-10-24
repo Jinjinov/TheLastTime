dab3f51618ee3b4fe11f286b2a3dc00fa4c79bd4
Urban <dzindzinovic@yahoo.com>
2021-10-24 17:54:04 +0200

    Version 1.0.0.1

---
+++ TheLastTime.Shared/Components/Goals/GoalListComponent.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using TheLastTime.Shared.Data;
+using TheLastTime.Shared.Models;
+
+namespace TheLastTime.Shared.Components.Goals
+{
+    public partial class GoalListComponent
+    {
+        [Inject]
+        DataService DataService { get; set; } = null!;
+
+        [Inject]
+        State State { get; set; } = null!;
+
+        [Inject]
+        ThemeOptions Theme { get; set; } = null!;
+
+        protected override void OnInitialized()
+        {
+            DataService.PropertyChanged += PropertyChanged;
+            State.PropertyChanged += PropertyChanged;
+        }
+
+        void PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            DataService.PropertyChanged -= PropertyChanged;
+            State.PropertyChanged -= PropertyChanged;
+        }
+
+        async Task NewGoal()
+        {
+            Goal goal = new Goal();
+
+            await DataService.Save(goal);
+        }
+    }
+}
